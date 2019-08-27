8e733055c4d89b406b0e5ebad0d18a639125caa9
xijunfei <xijunfei@qq.com>
2019-08-27 18:05:04 +0800

    * loot

--- Assets/Scenes/Test.cs
+++ Assets/Scenes/Test.cs
@@ -15,6 +15,13 @@ public class Test : MonoBehaviour
     void Start()
     {
         UnityEngine.Application.targetFrameRate = 30;
+        //AutoJob jump = new AutoJob();
+        //jump.Job = delegate ()
+        //{
+        //    AutoJob.SetCursorPos(1920 / 2, 1080 / 2);
+        //};
+        //jump.Run();
+
         AutoJob jump = new AutoJob();
         jump.Job = delegate ()
         {
@@ -30,6 +37,23 @@ public class Test : MonoBehaviour
             AutoJob.ClickKey(KeyCode.Alpha2);
         };
         attack.Run();
+
+        AutoJob loot = new AutoJob();
+        loot.RandomMin = 1f;
+        loot.RandomMax = 2f;
+        loot.Job = delegate ()
+        {
+
+            for (int i = -400; i < 400; i += 20)
+            {
+                for (int j = -400; j < 400; j += 20)
+                {
+                    AutoJob.SetCursorPos(1920/2 + i, 1080 / 2 + j);
+                    AutoJob.ClickKey(KeyCode.Mouse1);
+                }
+            }
+        };
+        loot.Run();
 
     }
 }
